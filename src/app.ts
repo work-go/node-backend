3ec932736fa1ca86d0385fad061da26653417e28
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2024-10-06 17:56:58 +0200

    created basic fastify app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("hello");
+import fastify from "fastify";
+
+const app = fastify({ logger: true });
+
+export const logger = app.log;
+
+app.get("/", (request, reply) => {
+  return "Hello World";
+});
+
+// app.post("/login")
+
+app.get("/users", () => {
+  return "Users";
+});
+
+const bootstrap = async () => {
+  try {
+    const dest = await app.listen({ host: "0.0.0.0", port: 3000 });
+    logger.info(`✅ Server listing on ${dest}`);
+  } catch (error) {
+    logger.error(
+      `❗ Failed to start server: ${
+        error instanceof Error ? error.message : "Uknown error occured"
+      }`
+    );
+  }
+};
+
+bootstrap();
