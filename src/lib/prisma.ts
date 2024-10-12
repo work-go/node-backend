df4386bcb4e6273a5aeecc328b22fcb87d4b3daf
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2024-10-12 22:02:14 +0200

    environmentized prisma connection url

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
+
 export const prisma = new PrismaClient({
   errorFormat: "pretty",
-  datasourceUrl: "postgresql://admin:password@postgres:5432/workgo",
+  datasourceUrl: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}`,
 });
